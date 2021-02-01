bac663bcb53f4b33e5d89161af154aa1e6b369f2
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-31 20:01:50 -0500

    opening mail on click, still storing items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,13 @@ let submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", function(event){
     event.preventDefault();
+    var maillink = "mailto:jacqueline.ross09@gmail.com"
     let contactme = {
         userName: document.getElementById("name").value, 
         userEmail: document.getElementById("email").value,
         userMessage: document.getElementById("message").value
     }
     localStorage.setItem("contactme", JSON.stringify(contactme)); 
+    window.location.href = maillink; 
     
 });
