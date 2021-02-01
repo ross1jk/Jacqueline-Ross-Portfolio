75a758f2e368ed1af2fe3d22be391c7a9560a3c0
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-01 12:59:39 -0500

    fixed conflicting ids

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,8 @@ let submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", function(event){
     event.preventDefault();
-    let nameInput = document.getElementById("name").value; 
-    let emailInput = document.getElementById("email").value;
+    let nameInput = document.getElementById("username").value; 
+    let emailInput = document.getElementById("email").value; 
     let message = document.getElementById("message").value; 
     var maillink = "mailto:jacqueline.ross09@gmail.com?subject=Portfolio Response From:"+emailInput+"&body="+message+" -"+nameInput+""
     let contactme = {
